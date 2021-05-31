93e579726aa7d5be75eac283a48ae6a6ae925d3a
Denis Lafontant <icebox827@gmail.com>
2021-05-30 20:24:28 -0400

    Create booked car reducer

---
+++ src/reducers/bookedCarReducer.js
@@ -0,0 +1,38 @@
+import {
+FETCH_BOOKED_CARS_REQUEST,
+FETCH_BOOKED_CARS_SUCCESS,
+FETCH_BOOKED_CARS_FAILURE,
+} from '../actions/action';
+
+const INITIAL_STATE = {
+  bookedCar: [],
+  loading: false,
+  error: null,
+};
+
+const bookedCarReducer = (state = INITIAL_STATE, action) => {
+  switch(action.type) {
+    case FETCH_BOOKED_CARS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case FETCH_BOOKED_CARS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        bookedCar: action.payload,
+      };
+    case FETCH_BOOKED_CARS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default bookedCarReducer;
