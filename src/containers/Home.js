b0e703ca2f29659925f28a7818a08329faf4275a
Denis Lafontant <icebox827@gmail.com>
2021-05-31 16:24:07 -0400

    Add assets

---
+++ src/containers/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Jumbotron, Container } from 'react-bootstrap';
+import '../styles/App.css';
+import Logo from '../assets/Logo.png'
+
+const Home = () => {
+  return (
+    <Jumbotron className="jumbotron">
+      <Container>
+
+      </Container>
+    </Jumbotron>
+  )
+};
+
+export default Home;
