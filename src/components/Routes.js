82f5e090d2017b9c449b7afa36329edd8d9d6f01
Denis Lafontant <icebox827@gmail.com>
2021-05-31 12:24:13 -0400

    Creates routes and navigation

--- src/components/Routes.js
+++ src/components/Routes.js
@@ -2,12 +2,23 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import NavBar from '../containers/Navbar';
+import Home from '../containers/Home';
+import Cars from '../containers/Cars';
+import Reservation from '../containers/Reservation';
+import '../styles/App.css';
 import store from '../store';
 
 const Routes = () => {
   return (
-    <Provider>
-      
+    <Provider store={store}>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/reservation' component={Reservation} />
+          <Route exact path='car' component={Cars}  />
+        </Switch>
+      </Router>
     </Provider>
   )
   
