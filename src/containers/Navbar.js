256f40bc9b5d7a852edcd367426b94038b83134f
Denis Lafontant <icebox827@gmail.com>
2021-05-31 18:21:26 -0400

    Update MenuItem in navbar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -5,20 +5,49 @@ import Logo from '../assets/Logo.png';
 const NavBar = () => {
   return (
     <Flex>
-      <Box p="2">
-        <Heading size="md">
-          <img src={Logo} alt="logo" className="logo" />
-        </Heading>
-      </Box>
-      <Spacer />
-      <Box>
-        <Button colorScheme="teal" mr="4">
-          <Link to="/signup">Sign Up</Link>
-        </Button>
-        <Button colorScheme="teal">
-        <Link to="/login">Log in</Link>
-        </Button>
-      </Box>
+      <Menu>
+        <Box p="2">
+          <Heading size="md">
+            <img src={Logo} alt="logo" className="logo" />
+          </Heading>
+        </Box>
+        <Box>
+          <MenuItem color="white" className="link" bg="blue">
+             <Link to="/">
+               <Text display="block">
+                  Home
+              </Text>
+             </Link>
+          </MenuItem>
+        </Box>
+        <Box>
+          <MenuItem color="white" className="link" bg="blue">
+            <Link to="/car">
+                <Text display="block">
+                  Car
+                </Text>
+            </Link>
+          </MenuItem>
+        </Box>
+        <Box>
+          <MenuItem color="white" className="link" bg="blue">
+           <Link to="/reservation">
+             <Text display="block">
+                Reservation
+              </Text>
+            </Link>
+          </MenuItem>
+        </Box>
+        <Spacer />
+        <Box>
+          <Button colorScheme="teal" mr="4">
+            <Link to="/signup">Sign Up</Link>
+          </Button>
+          <Button colorScheme="teal">
+          <Link to="/login">Log in</Link>
+          </Button>
+        </Box>
+      </Menu>
     </Flex>
     // <Menu className="navbar">
     //   <Grid templateColumns="repeat(5, 1fr)">
