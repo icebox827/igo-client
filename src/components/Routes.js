f3f00a84b6487d93563b21f0807794fe5c86a102
Denis Lafontant <icebox827@gmail.com>
2021-05-31 12:09:00 -0400

    Update Routes component

---
+++ src/components/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavBar from '../containers/Navbar';
+import store from '../store';
+
+const Routes = () => {
+  return (
+    <Provider>
+      
+    </Provider>
+  )
+  
+};
+
+export default Routes;
