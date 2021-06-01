8d025b61ee02168a4a1d3595b54c65dc9b8544d1
Denis Lafontant <icebox827@gmail.com>
2021-06-01 18:34:05 -0400

    Style book ride component

--- src/components/BookRide.js
+++ src/components/BookRide.js
@@ -5,7 +5,7 @@ import { Text, Button } from '@chakra-ui/react';
 const BookRide  = () => {
   return (
     <section className='call-action'>
-      <Text> Drive the Excellence </Text>
+      <Text fontSize="2xl" color="white" mt="4" fontWeight="bold"> Drive the Excellence </Text>
       <Button fontSize="2xl" color="red.500" mt="4" textAlign="center" fontWeight="bold">
         <Link className='btn btn-oval' to='/car'>
           Book your ride
