b310a9a627f55d969fa4c36d54f867151a842b1f
Denis Lafontant <icebox827@gmail.com>
2021-06-01 18:57:20 -0400

    Add new images

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Jumbotron, Container } from 'react-bootstrap';
 import { Text } from '@chakra-ui/react';
 import '../styles/App.css';
-import car from '../assets/car.jpeg'
+import carshow from '../assets/carshow.jpeg'
 import BookRide from '../components/BookRide';
 
 const Home = () => {
   return (
     <Jumbotron>
       <Container>
-        <img src={car} alt="logo" className="rentcar" />
+        <img src={carshow} alt="logo" className="rentcar" />
         <BookRide />
         <Text fontSize="4xl" color="blue.500" mt="4" textAlign="center" fontWeight="bold">
           The right place to book your favorite car
