b0ffc0289fc3b8b72bbd65cfa39701c629652839
Denis Lafontant <icebox827@gmail.com>
2021-06-02 18:46:32 -0400

    Update car detail

---
+++ src/containers/CarDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchStockItem } from '../actions/index';
+import { Jumbotron, Container } from 'react-bootstrap';
+import PropTypes from 'prop-types';
+import { useParams } from 'react-router';
+
+const CarDetail = () => {
+  
+};
+
+export default CarDetail;
