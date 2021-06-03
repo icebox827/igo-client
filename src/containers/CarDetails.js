010ea947c4f19f8422792ae3468ff0e6359513dc
Denis Lafontant <icebox827@gmail.com>
2021-06-03 15:49:24 -0400

    Add event to cardetail

--- src/containers/CarDetails.js
+++ src/containers/CarDetails.js
@@ -1,20 +1,40 @@
 import React from 'react';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCarItem } from '../actions/index';
 import { GridItem, Text, Image, Button, Grid, Box } from '@chakra-ui/react';
 import { useParams } from 'react-router';
 import Loader from '../components/Loader';
 import Error from '../components/Error'
+import { useHistory } from 'react-router';
 
 const CarDetail = () => {
+  const history = useHistory();
   const dispatch = useDispatch();
   const { carItem, loading, error } = useSelector((state) => state.carItem);
   const { id } = useParams();
+  const [user] = useState();
 
   useEffect(() => {
     dispatch(fetchCarItem(id));
   },[dispatch])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch('https://igo-api.herokuapp.com/api/v1/booked_cars/', 
+      {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'post',
+        body: JSON.stringify(user)
+      }
+    )
+    .then(res => res.json())
+    .then(data => {
+      if (data.message) {
+        history.push('/')
+      }
+    });
+  };
 
   if (loading) {
     return (
@@ -41,14 +61,14 @@ const CarDetail = () => {
         />
       </Box>
       <Box>
-      <Text fontSize="2xl" color="blue.500" mt="2" textAlign="center" fontWeight="bold">{carItem.make} {carItem.model}</Text>
+        <Text fontSize="2xl" color="blue.500" mt="2" textAlign="center" fontWeight="bold">{carItem.make} {carItem.model}</Text>
         <Text textAlign="center" >year : {carItem.year}</Text>
         <Text textAlign="center">Color: {carItem.color}</Text>
         <Text textAlign="center">Transmission: {carItem.transmission}</Text>
         <Text textAlign="center">Seats: {carItem.seats}</Text>
         <br />
         <br />
-        <Button href="" target="__blank" rel="noopener" className="favorite" colorScheme="teal">
+        <Button href="" target="__blank" rel="noopener" className="favorite" colorScheme="teal" onSubmit={handleSubmit}>
           {' '}
           Book now
         </Button>
--- src/containers/Reservation.js
+++ src/containers/Reservation.js
@@ -7,7 +7,6 @@ import { Card } from 'react-bootstrap';
 import { useParams } from 'react-router';
 import Loader from '../components/Loader';
 import Error from '../components/Error'
-import bookedCarReducer from '../reducers/bookedCarReducer';
 
 const Reservation = () => {
   const dispatch = useDispatch();
@@ -33,12 +32,24 @@ const Reservation = () => {
     );
   }
 
-  return bookedCar.map((bcar) => {
-    <Card style={{ width: '21rem' }} className="bg-dark text-white mt-3" key={bcar.id}>
-      
+  return (
+    <Card style={{ width: '21rem' }} className="bg-dark text-white mt-3" key={bookedCar.id}>
+      <Card.Img variant="top" src={bookedCar.image_url} />
+        <Card.Body>
+          <Card.Title className="make"><Text fontSize="2xl" color="blue.500" mt="2" textAlign="center" fontWeight="bold">{bookedCar.make} {bookedCar.model}</Text></Card.Title>
+          <Card.Text className="car">
+            <Text>{bookedCar.user_id} {bookedCar.car_id} </Text>
+            <Text>year : {bookedCar.year}</Text>
+            <hr/>
+            <Text>Color: {bookedCar.color}</Text>
+            <hr/>
+            <Text>Transmission: {bookedCar.transmission}</Text>
+            <hr/>
+            <Text>Seats: {bookedCar.seats}</Text>
+          </Card.Text>
+        </Card.Body>
     </Card>
-  })
-
+  )
 };
 
 export default Reservation;
