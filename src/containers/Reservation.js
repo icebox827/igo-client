7bbe285c9c45bd7f8e9c9faf7de2562678db2213
Denis Lafontant <icebox827@gmail.com>
2021-06-03 12:25:45 -0400

    Update reservation container

---
+++ src/containers/Reservation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchBookedCar } from '../actions/index';
+
+const Reservation = () => {
+
+};
+
+export default Reservation;
