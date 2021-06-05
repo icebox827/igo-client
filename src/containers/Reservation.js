b3b4f1f83f2a9f3d7c2bc17085ea52909e535198
Denis Lafontant <icebox827@gmail.com>
2021-06-04 21:47:09 -0400

    Add porp validation

--- src/containers/Reservation.js
+++ src/containers/Reservation.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchBookedcar, fetchBookedCar } from '../actions/index';
-import { GridItem, Text, Image, Button, Grid, Box } from '@chakra-ui/react';
+import { fetchBookedcar } from '../actions/index';
+import { GridItem, Text } from '@chakra-ui/react';
 import { Card } from 'react-bootstrap';
-import { useParams } from 'react-router';
+import PropTypes from 'prop-types';
 import Loader from '../components/Loader';
 import Error from '../components/Error'
 
-const Reservation = () => {
+const Reservation = ({
+  make, 
+  model, 
+  year, 
+  color, 
+  transmission,
+  seats,
+  image_url
+  }) => {
   const dispatch = useDispatch();
   const { bookedCar, loading, error } = useSelector((state) => state.bookedCar);
 
@@ -32,7 +40,7 @@ const Reservation = () => {
   }
 
   return bookedCar.map((cars) => (
-    <Card style={{ width: '21rem' }} className="bg-dark text-white mt-3" key={cars.id}>
+    <Card style={{ width: '21rem' }} className="bg-dark text-white mt-3" key={cars.user_id}>
       <Card.Img variant="top" src={cars.image_url} />
       <Card.Body>
         <Card.Title className="make"><Text fontSize="2xl" color="blue.500" mt="2" textAlign="center" fontWeight="bold">{cars.make} {cars.model}</Text></Card.Title>
@@ -50,5 +58,15 @@ const Reservation = () => {
     </Card>
   ));
 };
+
+Reservation.porpTypes = {
+  make: PropTypes.string,
+  model: PropTypes.string,
+  year: PropTypes.number,
+  color: PropTypes.string,
+  transmission: PropTypes.string,
+  seats: PropTypes.number,
+  image_url: PropTypes.string
+}
 
 export default Reservation;
