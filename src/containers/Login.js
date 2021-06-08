882c7c11b4c8015ba805d50b007af6075dd195d4
Denis Lafontant <icebox827@gmail.com>
2021-06-08 18:50:27 -0400

    Update authentication

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('https://igo-api.herokuapp.com/api/v1/login',
+    const response = await fetch('https://igo-api.herokuapp.com/api/v1/authentication',
       {
         headers: { 'Content-Type': 'application/json' },
         method: 'post',
